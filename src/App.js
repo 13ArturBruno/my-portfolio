0fb382f6d5eb026c639293a3cdd3d490f04c6981
Z_Usuario Temporario da Pos-graducao <posgrad@7setlabs.local>
2019-02-25 20:29:43 -0300

    testing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import fb from './assets/fb.png';
+import lin from './assets/lin.png';
+import tt from './assets/tt.png';
+import gm from './assets/gm.png';
+import gt from './assets/gt.png';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-           " * Artur Bruno * "
-          </p>
+          <text className="name">
+           Artur Bruno de Sena Santana
+          </text>
         </header>
+        <div className="social">
+            <a href="https://stackoverflow.com/"><img src={fb} alt="" className="img" /></a>
+            <a href="https://stackoverflow.com/"><img src={lin} alt="" className="img" /></a>
+            <a href="https://stackoverflow.com/"><img src={gt} alt="" className="img3" /></a>
+            <a href="https://stackoverflow.com/"><img src={gm} alt="" className="img2" /></a>
+            <a href="https://stackoverflow.com/"><img src={tt} alt="" className="img2" /></a>
+        </div>
       </div>
     );
   }
